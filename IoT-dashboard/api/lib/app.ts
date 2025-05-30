e87bbb37fd75e4bfe87dc739404caef3ab386993
jjosz <tar.jakubjarosz@gmail.com>
2025-05-30 10:09:28 +0200

    Add files via upload

--- IoT-dashboard/api/lib/app.ts
+++ IoT-dashboard/api/lib/app.ts
@@ -3,6 +3,7 @@ import { config } from './config';
 import Controller from "./interfaces/controller.interface";
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 class App {
     public app: express.Application;
@@ -11,6 +12,7 @@ class App {
         this.app = express();
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
+        this.connectToDatabase();
     }
 
     private initializeControllers(controllers: Controller[]): void {
@@ -20,7 +22,11 @@ class App {
     }
     private initializeMiddlewares(): void {
         this.app.use(bodyParser.json());
-        this.app.use(morgan('dev'));
+        //this.app.use(morgan('dev'));
+        this.app.use((req, res, next) => {
+            console.log(`[${req.method} ${req.url} ${new Date().toISOString()}]`);
+            next();
+        });
     }
 
 
@@ -29,5 +35,34 @@ class App {
             console.log(`App listening on the port ${config.port}`);
         });
     }
+    private async connectToDatabase(): Promise<void> {
+        try {
+            await mongoose.connect(config.databaseUrl);
+            console.log('Connection with database established');
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+        }
+
+        mongoose.connection.on('error', (error) => {
+            console.error('MongoDB connection error:', error);
+        });
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
+
+        process.on('SIGINT', async () => {
+            await mongoose.connection.close();
+            console.log('MongoDB connection closed due to app termination');
+            process.exit(0);
+        });
+
+        process.on('SIGTERM', async () => {
+            await mongoose.connection.close();
+            console.log('MongoDB connection closed due to app termination');
+            process.exit(0);
+        });
+    }
+
 }
 export default App;
--- IoT-dashboard/api/lib/controllers/data.controller.ts
+++ IoT-dashboard/api/lib/controllers/data.controller.ts
@@ -1,11 +1,15 @@
 import Controller from '../interfaces/controller.interface';
 import { Request, Response, NextFunction, Router } from 'express';
+import {checkIdParam} from "../middlewares/deviceIdParam.middleware";
+import DataService from "../modules/services/data.service";
+import {config} from "../config";
 
 let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
 
 class DataController implements Controller {
     public path = '/api/data';
     public router = Router();
+    public dataService = new DataService();
 
     constructor() {
         this.initializeRoutes();
@@ -13,27 +17,59 @@ class DataController implements Controller {
 
     private initializeRoutes() {
         this.router.get(`${this.path}/latest`, this.getLatestReadingsFromAllDevices);
-        this.router.get(`${this.path}/:id`, this.getElement);
-        this.router.get(`${this.path}/:id/latest`, this.getLatestForId);
-        this.router.get(`${this.path}/:id/:num`, this.getFromIdInRange);
-        this.router.post(`${this.path}/:id`, this.addData);
+        this.router.get(`${this.path}/:id`, checkIdParam, this.getAllDeviceData);
+        this.router.get(`${this.path}/:id/latest`, checkIdParam,this.getLatestForId);
+        this.router.get(`${this.path}/:id/:num`, checkIdParam,this.getFromIdInRange);
+        this.router.post(`${this.path}/:id`,checkIdParam, this.addData);
         this.router.delete(`${this.path}/all`, this.deleteAll);
-        this.router.delete(`${this.path}/:id`, this.deleteOne);
+        this.router.delete(`${this.path}/:id`,checkIdParam, this.deleteOne);
     }
 
-    private getLatestReadingsFromAllDevices = async (request: Request, response: Response, next: NextFunction) => {
-        response.status(200).json(testArr);
-    };
-    private addData= async (request: Request, response: Response, next: NextFunction) => {
-        const { elem } = request.body;
+    private getAllDeviceData = async (request: Request, response: Response, next: NextFunction) => {
         const { id } = request.params;
-        console.log(elem);
-        if(!isNaN(Number(elem))) {
-            testArr.push(elem);
+        const allData = await this.dataService.query(id);
+        response.status(200).json(allData);
+    };
+
+    private addData = async (request: Request, response: Response, next: NextFunction) => {
+        const { air } = request.body;
+        const { id } = request.params;
+
+        const data = {
+            temperature: air[0].value,
+            pressure: air[1].value,
+            humidity: air[2].value,
+            deviceId: Number(id),
+            readingDate : new Date()
         }
 
-        response.status(200).json(testArr);
+        try {
+
+            await this.dataService.createData(data);
+            response.status(200).json(data);
+        } catch (error) {
+            console.error(`Validation Error: ${error.message}`);
+            response.status(400).json({ error: 'Invalid input data.' });
+        }
     };
+    private getLatestReadingsFromAllDevices = async (request: Request, response: Response, next: NextFunction) => {
+        const allData = await this.dataService.getAllNewest();
+        response.status(200).json(allData);
+    };
+
+
+   // private addData= async (request: Request, response: Response, next: NextFunction) => {
+   //     const { elem } = request.body;
+   //     const { id } = request.params;
+    //    console.log(elem);
+    //    if(!isNaN(Number(elem))) {
+    //        testArr.push(elem);
+   //     }
+//
+    //    response.status(200).json(testArr);
+   // };
+
+
     private getElement = async (request: Request, response: Response, next: NextFunction) => {
         const { id } = request.params;
         console.log(Number(id));
@@ -47,8 +83,9 @@ class DataController implements Controller {
     };
     private getLatestForId = async (request: Request, response: Response, next: NextFunction) => {
         const { id } = request.params;
+        const data = await this.dataService.get(id);
 
-        response.status(200).json(Math.max(...testArr));
+        response.status(200).json(data);
     };
     private getFromIdInRange = async (request: Request, response: Response, next: NextFunction) => {
         const { id } = request.params;
@@ -56,15 +93,16 @@ class DataController implements Controller {
         response.status(200).json(testArr.length);
     };
     private deleteAll = async (request: Request, response: Response, next: NextFunction) => {
-        testArr = [];
-        response.status(200).json(testArr);
+        for(let i = 0; i<config.supportedDevicesNum;i++)
+        {
+            await this.dataService.deleteData(String(i));
+        }
+        response.status(200).json(await this.dataService.getAllNewest());
     };
     private deleteOne = async (request: Request, response: Response, next: NextFunction) => {
         const { id } = request.params;
-        if(!isNaN(parseInt(id))) {
-            testArr.splice(parseInt(id), 1);
-        }
-        response.status(200).json(testArr);
+        const allData = await this.dataService.deleteData(id);
+        response.status(200).json(allData);
     };
 }
 
